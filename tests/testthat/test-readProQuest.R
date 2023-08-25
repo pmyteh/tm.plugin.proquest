cad44c0d89bd8bcfc9ded41d562e050536fdb45f
Tom Nicholls <github@tomnicholls.me.uk>
2023-08-25 18:37:07 +0100

    Add test-readProQuest.R tests for new email format
    The format of each document seems to be the same as the previous format, but add an end-to-end test from email to PlainTextDocument to ensure no regressions later.

--- tests/testthat/test-readProQuest.R
+++ tests/testthat/test-readProQuest.R
@@ -94,6 +94,103 @@ test_that("specific parsing issues are solved", {
   expect_equal(count_missing("author"), 27)
 })
 
+test_that("v2-format document 1 is fully correct", {
+  library(lubridate)
+  doc <- system.file("emls", "format-v2.eml",
+                      package = "tm.plugin.proquest") %>%
+    ProQuestSource %>%
+    VCorpus %>%
+    `[[`(1)
+
+
+  expect_equal(meta(doc, "origin"),
+               "New York Times,  Late Edition (East Coast); New York, N.Y.")
+  expect_equal(meta(doc, "author"),
+               c("Wines, Michael",
+                 "Cramer, Maria")
+  )
+  expect_equal(meta(doc, "type"),
+               "News")
+  expect_equal(meta(doc, "section"),
+               character(0))
+  expect_equal(meta(doc, "graphic"),
+               character(0))
+  expect_equal(meta(doc, "correction"),
+               character(0))
+  expect_equal(meta(doc, "language"),
+               "en")
+  expect_equal(meta(doc, "wordcount"),
+               1214)
+  expect_equal(meta(doc, "intro"),
+               character(0))
+  expect_equal(meta(doc, "people"),
+               character(0))
+  expect_equal(meta(doc, "subject"),
+               c("Hispanic Americans",
+                 "Ethnicity",
+                 "Minority & ethnic groups",
+                 "Coronaviruses",
+                 "Pandemics",
+                 "Native North Americans",
+                 "Census of Population",
+                 "COVID-19" )
+  )
+  #  expect_equal(meta(doc, "coverage"),
+  #               "")
+  expect_equal(meta(doc, "company"),
+               c("New York Times Co",
+                 "Bureau of the Census",
+                 "House of Representatives")
+  )
+  expect_equal(meta(doc, "rights"),
+               "Copyright New York Times Company Mar 10, 2022")
+  expect_equal(meta(doc, "heading"),
+               "2020 Census Undercounted Hispanic, Black and Native American Residents")
+  expect_equal(meta(doc, "datetimestamp"),
+               lubridate::parse_date_time("2022-03-10", "ymd"))
+  expect_equal(meta(doc, "publisher"),
+               "New York Times Company")
+  #  expect_equal(meta(doc, "edition"),
+  #               "")
+  expect_equal(meta(doc, "id"),
+               "MSTAR_2637783411")
+  expect_equal(meta(doc, "page"),
+               character(0))
+
+  expect_equal(content(doc),
+               c("WASHINGTON — Saddled with daunting logistical and political obstacles, the 2020 census seriously undercounted the number of Hispanic, Black and Native American residents even though its overall population count was largely accurate, the Census Bureau said Thursday.",
+                 "At the same time, the census overcounted white and Asian American residents, the bureau said.",
+                 "In essence, the bureau’s report said, minority groups — mostly concentrated in cities and tribal areas — were underrepresented in census figures, even though the total population count in those areas often was fairly accurate. That could affect those groups’ political clout, and conceivably could sway decisions by businesses and governments over the next decade, from the allocation of city services to locations of stores.",
+                 "Some minority advocacy groups threatened to challenge the results in court, but remedying the undercounts would be difficult if not impossible, experts said.",
+                 "Robert Santos, the bureau’s director, said that despite the omissions, the results were consistent with recent censuses.",
+                 "“This is notable, given the unprecedented challenges of 2020,” he said in a statement. “But the results also include some limitations; the 2020 census undercounted many of the same population groups we have historically undercounted, and it overcounted others.”",
+                 "Those challenges included the coronavirus pandemic, which hamstrung efforts to interview people in person, and interference by the Trump administration, which tried to remove noncitizens from the count and, later, to end the counting process weeks early.",
+                 "But some obstacles face every census count. The census has historically undercounted populations that are harder to reach through surveys, phone calls and door-to-door canvassing, including Native Americans on reservations, poor urban communities and immigrants living in the country illegally.",
+                 "The estimates released Thursday covered the count of 323.2 million people who were living in households on April 1, 2020, the official census date. Counts of others, such as prison inmates and students in college dormitories, pushed the total population count to 331.45 million.\n",
+                 "\nTerri Ann Lowenthal, a leading expert on the census and consultant to governments and others with a stake in the count, called the results “troubling but not entirely surprising.”",
+                 "“Overall, the results are less accurate than in 2010,” she said.",
+                 "\nBy the bureau’s estimates, the 2020 tally incorrectly counted 18.8 million residents, double-counting some 5.2 million people, wrongly including another 2 million and missing others entirely, even as it came extremely close to reaching an accurate count of the overall population. Many of the people whom the census originally failed to count were picked up and added to final census totals through a process called imputation — a statistical guess, using complex algorithms, of who was living in places that census takers could not reach.",
+                 "Those imputations, however, did not guess the race or ethnicity of missing residents. That partly explains why the grand total count of residents was substantially accurate, even as many minorities were missed.",
+                 "Although the bureau did not say how many people it missed entirely, they were mostly people of color, disproportionately young ones. The census missed counting 4.99 of every 100 Hispanics, 5.64 of every 100 Native Americans and 3.3 of every 100 African Americans.",
+                 "In contrast, for every 100 residents counted, the census wrongly added 1.64 non-Hispanic whites and 2.62 ethnic Asians.",
+                 "Young people in general were undercounted: by 2.79% for children under 5, and, among those ages 18 to 29, 2.25% for men and 0.98% for women. Older residents were overcounted, most significantly by 2.63% for women 50 and older.",
+                 "The estimates released Thursday — in essence, a statistical adjustment of totals made public last year — are based on an examination of federal records and an extensive survey in which the bureau interviewed residents in some 10,000 census blocks — the smallest unit used in census tabulations. Bureau experts then compared their answers to the actual census results for those blocks.",
+                 "The survey enabled the bureau to estimate how many residents it missed entirely in the 2020 count, how many people were counted twice and how many people — such as short-term visitors and people who died before or were born after the official April 1, 2020, census date — were counted mistakenly.",
+                 "Some advocacy groups for minority populations expressed outrage at the undercounts. In a news release, Arturo Vargas, the CEO of the NALEO Educational Fund, which represents Latinos, called the counting errors a “five-alarm fire” and questioned whether the results should be used.",
+                 "Marc Morial, the CEO of the National Urban League, called the undercount of Black people a tragedy, adding, “Any options we have, including litigation, will be on the table.”",
+                 "Experts said, however, that those unhappy with the results had limited options. Earlier census figures already have been used to reapportion seats in the House of Representatives and state legislatures, among other governing bodies.",
+                 "The estimates released Thursday do not affect redistricting. They reflect only errors in the national population count, and final refinements, due this summer, would only break down wrong counts by state.",
+                 "While they decried the scope of the flaws in the 2020 census, some experts noted that many undercounts and other errors were not statistically different from those in the 2010 count. And that, they said, was a triumph all by itself.\n",
+                 "\nThe most intriguing question is how the Census Bureau managed such an accurate count given the hurdles it faced, said Steve Jost, a former census official and consultant to the Census Project, a coalition of organizations that advocates an accurate count.",
+                 "“To pull off the nation’s largest peacetime mobilization in the middle of a once-a-century pandemic and operational interference from the White House is a remarkable achievement,” he said.",
+                 "\nThe 2020 census faced a series of challenges. The coronavirus pandemic shut down much of the count just as it was beginning in April 2020, forcing the bureau to extend its work by nearly two months. Later in the year, wildfires in the West and coastal hurricanes upended the bureau’s work just as door-knockers were fanning out to survey millions of households that had not filled out their forms.",
+                 "The Trump administration later moved up the deadline to finish the counting, raising concerns about an undercount. The problems led many experts, including some senior Census Bureau officials, to worry that the final count would be fatally flawed.",
+                 "In September, a 59-page analysis of the 2020 census commissioned and reviewed by the American Statistical Association said the count appeared accurate enough for its overriding constitutional purpose: reallocating the 435 seats in the House of Representatives.",
+                 "But the experts who drafted the report limited their findings to the overall national tally and counts in the 50 states and the District of Columbia. Much more study would be needed, they said, to gauge the reliability of local population totals and characteristics such as race and ethnicity that are vital parts of every census.\nThis article originally appeared in The New York Times.\n"       )
+  )
+})
+
+
 test_that("forwarded emails parse", {
   corp <- system.file("emls", "forwarded.eml",
                       package = "tm.plugin.proquest") %>%

8052d079ec373ed1b0c513c75132f7d994eee332
Tom Nicholls <github@tomnicholls.me.uk>
2023-08-25 18:07:16 +0100

    Add new ("v2") format anonymised email test file and check it parses right

--- tests/testthat/test-ProQuestSource.R
+++ tests/testthat/test-ProQuestSource.R
@@ -12,6 +12,14 @@ test_that("forwarded emails load", {
   expect_length(s$content, 100)
 })
 
+test_that("the correct number of documents are loaded from the v2-format file", {
+  file <- system.file("emls", "format-v2.eml",
+                      package = "tm.plugin.proquest")
+  s <- ProQuestSource(file)
+  expect_length(s$content, 20)
+})
+
+
 # If the mangled <html> tag beginning the first article isn't corrected, the
 # parser will treat that article's closing </html> as an end of the whole
 # document and return only the first article.
